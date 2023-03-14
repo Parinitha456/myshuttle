08e9b4d07e9762dfd6ae856580fb0978dc8f4e49
Krishna Chavan <krishna.chavan@brilliompn.onmicrosoft.com>
2023-03-14 04:59:34 +0000

    Updated eks-worker-nodes.tf

--- IAC AWS/eks-worker-nodes.tf
+++ IAC AWS/eks-worker-nodes.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryR
 
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "demo"
+  node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
 
