da1ef19f18634cae1d31248fbb7a7e86e6e5a353
Parinitha G <parinitha.g@brilliompn.onmicrosoft.com>
2023-03-15 11:20:58 +0000

    Updated variable.tf

--- IAC AWS/variable.tf
+++ IAC AWS/variable.tf
@@ -7,3 +7,22 @@ variable "cluster-name" {
   type = string
   description = "Cluster name of AKS"
 }
+
+variable "access_key"{
+  type = string
+  description = "Access key"
+}
+
+variable "secret_key"{
+  type = string
+  description = "Secrete key"
+}
+
+variable "demo-nodename"{
+  type = string
+  description = "demo-nodename"
+}
+variable "node_group_name"{
+  type = string
+  description = "node_group_name"
+}
